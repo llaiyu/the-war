bc2189a8e1fcd7ba04a96376cf470763f8333942
lilingtao <lilingtao@digiproto.com>
2021-02-07 17:49:51 +0800

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,99 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Play />
+        <WarBoard />       
+      </div>
+    );
+  };
+}
+
+// class Settings extends React.Component {
+//   constructor(props) {
+//     this.state = {
+//       isOpened : false
+//     }
+//   }
+
+//   openModal = () => {
+//     this.setState({ isOpened: true });
+//   }
+ 
+//   closeModal = () => {
+//     this.setState({isOpened: false });
+//   }
+
+//   render() {
+//     return(
+//       <Modal isOpen={this.state.isOpened}>
+//         <div>
+//           Hello Modal！
+//         </div>
+//         <button onClick={this.closeModal}>Cancel</button>
+//       </Modal>
+//     );
+//   };
+// }
+
+class Play extends React.Component {
+
+  handleClick() {
+    // DialogCustom.show({ onOk: this.handleOk });
+  }
+
+  render() {
+    return(
+      <button className="playbutton" onClick="handleClick">Play</button>
+    );
+  };
+}
+
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class WarBoard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.props = {
+      scope : 10
+    };
+  }
+  
+  
+
+  renderSquare() {
+    return <Square />;
+  }
+
+  render() {
+    let lengthList = Array(100).fill(0);
+    const row = lengthList.map((num) => <Square />);
+    const table = lengthList.map((num) => <div className="board-row">{row}</div>);
+    return ( 
+      <div>
+        {table}
+      </div>);
+  };
+}
+
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/soldier.js
@@ -0,0 +1,52 @@
+
+
+
+class SoilderData {
+    constructor(blood,defense,damage,money) {
+        this.blood = blood;
+        this.defense = defense;
+        this.damage = damage;
+        this.money = money;
+    }
+}
+
+
+class Soldier{
+    constructor(props){
+        super(props);
+        
+    }
+}
+
+// 骑兵
+class Cavalry extends Soldier {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : new SoilderData(),
+            strikingDistance : 1
+            
+        };
+    }
+}
+
+// 弓箭兵
+class Archers extends Soldier {
+    constructor(props){
+        super(props);
+        this.state = {data : new SoilderData(),strikingDistance : 1};
+    }
+}
+
+//步兵
+class Infantry extends Soldier {
+    constructor(props){
+        super(props);
+        this.state = {data : new SoilderData(),strikingDistance : 1};
+    }
+}
+
+function test() {
+
+}
+
